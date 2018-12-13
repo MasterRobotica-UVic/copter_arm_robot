b9d107feb13a0c97ee949ff82393c8d071328249
Carlos J. Rosales Gallegos <carlos@beta-robots.com>
2018-12-13 11:40:12 +0100

    Makes the motor turns in both directions according to controller output
    Also cleans a bit the comments in hwiface

--- firmware/fan_arduino.cpp
+++ firmware/fan_arduino.cpp
@@ -4,50 +4,98 @@
 	#include <WProgram.h>
 #endif
 
-#include <std_msgs/UInt8.h>
-#include <std_msgs/UInt16.h>
+#include <std_msgs/Int16.h>
 #include <ros/time.h>
 #include <ros.h>
 
-int potPin = 0;
-int enablePin = 2;
-std_msgs::UInt16 angle_;
+int msrPin = 2;
+int directionPin = 29;
+int cmdPin = 3;
+std_msgs::Int16 angle_;
+int directDrivePin = 0;
+int inputPin = 1;
+
+// debug
+std_msgs::Int16 cmd_mapped_;
 
 ros::NodeHandle nh;
 
-void updateCommand(const std_msgs::UInt8& cmd_msg)
+void updateCommand(const std_msgs::Int16& cmd_msg)
 {
-	digitalWrite(enablePin, HIGH);
-	analogWrite(3, cmd_msg.data);
+	// convert from signed to unsigned with direction
+	int cmd;
+	if( cmd_msg.data > 0 )
+	{
+		digitalWrite(directionPin, HIGH);
+		cmd = map(cmd_msg.data, 0, 255, 0, 255);
+	}
+	else
+	{
+		digitalWrite(directionPin, LOW);
+		cmd = map(cmd_msg.data, -255, 0, 255, 0);
+	}
+	cmd_mapped_.data = cmd;
+	analogWrite(cmdPin, cmd);
 }
 
 void updateAngle()
 {
-	angle_.data = analogRead(potPin);
+	angle_.data = analogRead(msrPin);
 }
 
-ros::Subscriber<std_msgs::UInt8> sub_command_("arduino_cmd", &updateCommand);
+ros::Subscriber<std_msgs::Int16> sub_command_("arduino_cmd", &updateCommand);
 ros::Publisher pub_angle_("potentiometer_msr", &angle_);
+ros::Publisher pub_cmd_mapped_("arduino_cmd_mapped", &cmd_mapped_);
 
 void setup()
 {
 	nh.initNode();
 	nh.subscribe(sub_command_);
 	nh.advertise(pub_angle_);
+	nh.advertise(pub_cmd_mapped_);
 
+	// try one more time after 2 seconds
 	while(!nh.connected())
 	{
 		nh.spinOnce();
-		delay(100);
+		delay(2000);
 	}
 
-	pinMode(enablePin, OUTPUT);
+	digitalWrite(directionPin, HIGH);
+	pinMode(directionPin, OUTPUT);
+	analogWrite(cmdPin, 0);
 }
 
 void loop()
 {
-	updateAngle();
-	pub_angle_.publish(&angle_);
-	nh.spinOnce();
-	delay(1);
+	// direct drive via joystick, only write if there is a significant inpu to not interfere with PID
+	int input = analogRead(directDrivePin);
+	int cmd;
+	if( input > 522 )
+	{
+		digitalWrite(directionPin, HIGH);
+		cmd = map(input, 522, 1024, 0, 255);
+		analogWrite(cmdPin, cmd);
+	}
+	else if ( input < 502 )
+	{
+		digitalWrite(directionPin, LOW);
+		cmd = map(input, 0, 502, 255, 0);
+		analogWrite(cmdPin, cmd);
+	}
+
+	// ros section
+	if( nh.connected() )
+	{
+		updateAngle();
+		pub_angle_.publish(&angle_);
+		pub_cmd_mapped_.publish(&cmd_mapped_);
+		nh.spinOnce();
+	}
+	else
+	{
+		analogWrite(cmdPin, 0);
+	}
+
+	delay(2);
 }
--- src/fan_hwiface.cpp
+++ src/fan_hwiface.cpp
@@ -8,8 +8,7 @@
 // ROS headers
 #include <ros/ros.h>
 #include <std_msgs/Duration.h>
-#include <std_msgs/UInt8.h>
-#include <std_msgs/UInt16.h>
+#include <std_msgs/Int16.h>
 #include <std_msgs/Bool.h>
 #include <urdf/model.h>
 
@@ -36,14 +35,19 @@ class COPTERHW : public hardware_interface::RobotHW
 public:
 
 	COPTERHW() {}
-	virtual ~COPTERHW() {}
+	~COPTERHW()
+	{
+		// to set PWM to zero
+		pub_commands_->msg_.data = static_cast<int16_t>(0);
+		pub_commands_->unlockAndPublish();
+	}
 
 	std::string robot_namespace_;
 
 	std::string urdf_string_;
 	urdf::Model urdf_model_;
 
-	realtime_tools::RealtimePublisher<std_msgs::UInt8> *pub_commands_;
+	realtime_tools::RealtimePublisher<std_msgs::Int16> *pub_commands_;
 	ros::Subscriber sub_measures_;
 
 
@@ -51,7 +55,7 @@ public:
 	{
 		n_joints_ = 2;
 
-		pub_commands_ = new realtime_tools::RealtimePublisher<std_msgs::UInt8>(nh, "arduino_cmd", 1);
+		pub_commands_ = new realtime_tools::RealtimePublisher<std_msgs::Int16>(nh, "arduino_cmd", 1);
 		sub_measures_ = nh.subscribe("potentiometer_msr", 1000, &COPTERHW::updateMsr, this) ;
 
 		nh.getParam("gain", gain_);
@@ -112,12 +116,11 @@ public:
 		{
 			// only actuate in the correct direction
 			double fan_speed = ( ( joint_velocity_command_.at(1) ) );
-			if( fan_speed > 0.0 )
-				fan_speed = 0;
-			fan_speed = std::abs(fan_speed);
-			pub_commands_->msg_.data = static_cast<uint8_t>(1023*fan_speed/5);
-			if( pub_commands_->msg_.data > 255 )
-				pub_commands_->msg_.data = 255;
+			pub_commands_->msg_.data = static_cast<int16_t>(fan_speed);
+			if( pub_commands_->msg_.data > 32767 )
+				pub_commands_->msg_.data = 32767;
+			else if( pub_commands_->msg_.data < -32767 )
+				pub_commands_->msg_.data = -32767;
 			pub_commands_->unlockAndPublish();
 		}
 		return;
@@ -142,7 +145,7 @@ public:
 	std::vector<std::string> joint_names_;
 
 	// limits
-	std::vector<double> 
+	std::vector<double>
 	joint_lower_limits_,
 	joint_upper_limits_;
 
@@ -168,35 +171,13 @@ public:
 
 private:
 
-	void updateMsr(const std_msgs::UInt16::ConstPtr& msg)
+	void updateMsr(const std_msgs::Int16::ConstPtr& msg)
 	{
 		angle_ = gain_* msg->data + bias_;
 	}
+};
 
-/*  // Get Transmissions from the URDF
-	bool parseTransmissionsFromURDF(const std::string& urdf_string);
-
-	// Register all interfaces using
-	void registerInterfaces(const urdf::Model *const urdf_model, 
-									 std::vector<transmission_interface::TransmissionInfo> transmissions);
-
-	// Initialize all KDL members
-	bool initKDLdescription(const urdf::Model *const urdf_model);
-
-	// Helper function to register limit interfaces
-	void registerJointLimits(const std::string& joint_name,
-									 const hardware_interface::JointHandle& joint_handle_effort,
-									 const hardware_interface::JointHandle& joint_handle_position,
-									 const hardware_interface::JointHandle& joint_handle_velocity,
-									 const hardware_interface::JointHandle& joint_handle_stiffness,
-									 const urdf::Model *const urdf_model,
-									 double *const lower_limit, double *const upper_limit,
-									 double *const lower_limit_stiffness, double *const upper_limit_stiffness,
-									 double *const effort_limit);*/
-
-}; // class
-
-} // namespace
+}
 
 bool g_quit = false;
 
@@ -207,92 +188,53 @@ void quitRequested(int sig)
 
 int main( int argc, char** argv )
 {
-	// initialize ROS
 	ros::init(argc, argv, "fan_hwiface", ros::init_options::NoSigintHandler);
 
-	// ros spinner
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
 
-	// custom signal handlers
 	signal(SIGTERM, quitRequested);
 	signal(SIGINT, quitRequested);
 	signal(SIGHUP, quitRequested);
 
-	// create a node
 	ros::NodeHandle copter_arm_nh;
 
-	// get params or give default values
 	std::string name;
 	copter_arm_nh.param("name", name, std::string("copter_arm"));
 
-	// advertise the e-stop topic
-	// ros::Subscriber estop_sub = copter_arm_nh.subscribe(copter_arm_nh.resolveName("emergency_stop"), 1, eStopCB);
-
-	// get the general robot description, the lwr class will take care of parsing what's useful to itself
-	// std::string urdf_string = getURDF(copter_arm_nh, "/robot_description");
-
-	// construct and start the real lwr
 	fan_hwiface::COPTERHW copter_arm_hwiface;
-	// copter_arm_hwiface.create(name, urdf_string);
 	if(!copter_arm_hwiface.init(copter_arm_nh, name))
 	{
 		ROS_FATAL_NAMED("lwr_hw","Could not initialize robot real interface");
 		return -1;
 	}
 
-	// timer variables
 	struct timespec ts = {0, 0};
 	ros::Time last(ts.tv_sec, ts.tv_nsec), now(ts.tv_sec, ts.tv_nsec);
 	ros::Duration period(1.0);
 
-	//the controller manager
 	controller_manager::ControllerManager manager(&copter_arm_hwiface, copter_arm_nh);
 
-	ros::Rate rate(100);
+	ros::Rate rate(150);
 	while( !g_quit )
 	{
-		// get the time / period
 		if (!clock_gettime(CLOCK_REALTIME, &ts))
 		{
 			now.sec = ts.tv_sec;
 			now.nsec = ts.tv_nsec;
 			period = now - last;
 			last = now;
-		} 
+		}
 		else
 		{
 			ROS_FATAL("Failed to poll realtime clock!");
 			break;
 		}
 
-		// read the state from the lwr
 		copter_arm_hwiface.read(ros::Time::now(), period);
 
-		// Compute the controller commands
 		bool resetControllers = false;
-		/*if(!wasStopHandled && !resetControllers)
-		{
-			ROS_WARN("E-STOP HAS BEEN PRESSED: Controllers will be restarted, but the robot won't move until you release the E-Stop");
-			ROS_WARN("HOW TO RELEASE E-STOP: rostopic pub -r 10 /NAMESPACE/emergency_stop std_msgs/Bool 'data: false'");
-			resetControllers = true;
-			wasStopHandled = true;
-		}
-
-		if( isStopPressed )
-		{
-			wasStopHandled = false;
-		}
-		else
-		{
-			resetControllers = false;
-			wasStopHandled = true;
-		}  */  
-
-		// update the controllers
 		manager.update(ros::Time::now(), period, resetControllers);
-
-		// write the command to the lwr
 		copter_arm_hwiface.write(ros::Time::now(), period);
 
 		rate.sleep();
