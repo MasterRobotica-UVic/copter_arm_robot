e639fc8e6dd4c1f66629cc7819c713e8b20ade6d
Carlos J. Rosales Gallegos <carlos@beta-robots.com>
2020-01-10 17:04:26 +0100

    Removes the use of the joystick, and switches back to UInt16 for all values

--- firmware/fan_arduino.cpp
+++ firmware/fan_arduino.cpp
@@ -1,118 +1,70 @@
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "Arduino.h"
+  #include "Arduino.h"
 #else
-	#include <WProgram.h>
+  #include <WProgram.h>
 #endif
 
-#include <std_msgs/Int16.h>
+#include <std_msgs/UInt16.h>
 #include <std_msgs/Float64.h>
 #include <ros/time.h>
 #include <ros.h>
 
-int msrPin = 2;
-int directionPin = 29;
-int cmdPin = 3;
-std_msgs::Int16 angle_;
-int directDrivePin = 0;
-int inputPin = 1;
-std_msgs::Float64 joystick_;
+int msrPin = A1;
+int directionPin = 2;
+int cmdPin = 9;
+std_msgs::UInt16 angle_;
 
-// debug
-std_msgs::Int16 cmd_mapped_;
-
-void updateCommand(const std_msgs::Int16& cmd_msg)
+void updateCommand(const std_msgs::UInt16& cmd_msg)
 {
-	// convert from signed to unsigned with direction
-	int cmd;
-	if( cmd_msg.data > 0 )
-	{
-		digitalWrite(directionPin, HIGH);
-		cmd = map(cmd_msg.data, 0, 255, 0, 255);
-	}
-	else
-	{
-		digitalWrite(directionPin, LOW);
-		cmd = map(cmd_msg.data, -255, 0, 255, 0);
-	}
-	cmd_mapped_.data = cmd;
-	analogWrite(cmdPin, cmd);
+  // ensure 0 to 255 value
+  int cmd;
+  //digitalWrite(directionPin, HIGH);
+  //cmd = map(cmd_msg.data, 0, 255, 0, 255);
+  analogWrite(cmdPin, cmd_msg.data);
 }
 
 ros::NodeHandle nh;
-ros::Subscriber<std_msgs::Int16> sub_command_("arduino_cmd", &updateCommand);
+ros::Subscriber<std_msgs::UInt16> sub_command_("arduino_cmd", &updateCommand);
 ros::Publisher pub_angle_("potentiometer_msr", &angle_);
-ros::Publisher pub_cmd_mapped_("arduino_cmd_mapped", &cmd_mapped_);
-ros::Publisher pub_joystick_setpoint_("joystick_setpoint", &joystick_);
 
-void updateAngle()
+void updateAndPublishAngle()
 {
-	angle_.data = analogRead(msrPin);
-}
-
-void updateJoystick()
-{
-	// from -90 to 90 degress, but in radians
-	joystick_.data = map(analogRead(inputPin), 0, 1024, -512, 512)*1.57/512.0;
-
-}
-
-void publish()
-{
-	pub_angle_.publish(&angle_);
-	pub_cmd_mapped_.publish(&cmd_mapped_);
-	pub_joystick_setpoint_.publish(&joystick_);
+  angle_.data = analogRead(msrPin);
+  pub_angle_.publish(&angle_);
 }
 
 void setup()
 {
-	nh.initNode();
-	nh.subscribe(sub_command_);
-	nh.advertise(pub_angle_);
-	nh.advertise(pub_cmd_mapped_);
-	nh.advertise(pub_joystick_setpoint_);
+  nh.initNode();
+  nh.subscribe(sub_command_);
+  nh.advertise(pub_angle_);
 
-	// try one more time after 2 seconds
-	while(!nh.connected())
-	{
-		nh.spinOnce();
-		delay(2000);
-	}
+  // try one more time after 2 seconds
+  while(!nh.connected())
+  {
+    nh.spinOnce();
+    delay(2000);
+  }
 
-	digitalWrite(directionPin, HIGH);
-	pinMode(directionPin, OUTPUT);
-	analogWrite(cmdPin, 0);
+  pinMode(directionPin, OUTPUT);
+  digitalWrite(directionPin, HIGH);
+  // see https://etechnophiles.com/change-frequency-pwm-pins-arduino-uno/
+  TCCR1B = TCCR1B & B11111000 | B00000001; // set timer 1 divisor to 1 for PWM frequency of 31372.55 Hz
+  analogWrite(cmdPin, 0);
 }
 
 void loop()
 {
-	// direct drive via joystick, only write if there is a significant inpu to not interfere with PID
-	int input = analogRead(directDrivePin);
-	int cmd;
-	if( input > 522 )
-	{
-		digitalWrite(directionPin, HIGH);
-		cmd = map(input, 522, 1024, 0, 255);
-		analogWrite(cmdPin, cmd);
-	}
-	else if ( input < 502 )
-	{
-		digitalWrite(directionPin, LOW);
-		cmd = map(input, 0, 502, 255, 0);
-		analogWrite(cmdPin, cmd);
-	}
+  // ros section
+  if( nh.connected() )
+  {
+    updateAndPublishAngle();
+    nh.spinOnce();
+  }
+  else
+  {
+    analogWrite(cmdPin, 0);
+  }
 
-	// ros section
-	if( nh.connected() )
-	{
-		updateAngle();
-		updateJoystick();
-		publish();
-		nh.spinOnce();
-	}
-	else
-	{
-		analogWrite(cmdPin, 0);
-	}
-
-	delay(2);
+  delay(2);
 }
